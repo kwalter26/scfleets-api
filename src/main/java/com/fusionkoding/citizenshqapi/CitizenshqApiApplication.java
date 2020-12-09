2c29332d389e3f3cfc83ae12ed4d6a63202ec32b
Kyle Walter <walter.kl26@gmail.com>
2020-12-09 00:00:58 -0600

    Initial Commit

---
+++ src/main/java/com/fusionkoding/citizenshqapi/CitizenshqApiApplication.java
@@ -0,0 +1,13 @@
+package com.fusionkoding.citizenshqapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenshqApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenshqApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fusionkoding/citizenshqapi/CitizenshqApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fusionkoding.citizenshqapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenshqApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
